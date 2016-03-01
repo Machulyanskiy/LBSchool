8927a188a695c03c632dc1d1e0a7439db0d2ebcd
Machulyanskiy <machulyanskiy.dmytro.wsk@gmail.com>
2016-03-01 02:51:46 +0200

    add styles

---
+++ www/application/views/land_page.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link href="/static/style/bootstrap.min.css" rel="stylesheet">
+	<link href="/static/style/styles.css" rel="stylesheet">
+	<title>LB_School</title>
+</head>
+<body>
+
+<nav class="navbar navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header"></div>
+	</div>
+</nav>
+</body>
+</html>
